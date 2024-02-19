24627e7124bb129249205119259ec19ea6cfc9fd
name <email>
2024-02-19 18:44:11 +0530

    two sum II

---
+++ Array/twoSum-II.js
@@ -0,0 +1,24 @@
+let arr = [2,5,7,9,11,45];
+let target = 9;
+
+let left = 0;
+let right= arr.length-1;
+
+let twosum2= function(arr ,target){
+    
+    while(left<right){
+        if(arr[left]+arr[right] == target){
+            return [left+1,right+1] ;  // 0 index to 1index;
+        }
+        else if( arr[left] + arr[right] > target){
+            right--;
+        }
+        else {
+            left++;
+        }
+    }
+  
+}
+  let result = twosum2(arr , target);
+console.log(result);
+ 
