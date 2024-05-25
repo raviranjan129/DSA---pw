8fa7c102800233a592c98f6bc04bbc34409b2578
name <rk6318633@gmail.com>
2024-05-26 00:45:40 +0530

    flip equivalent

--- Tree/BinaryTree/flipEquivalent.js
+++ Tree/BinaryTree/flipEquivalent.js
@@ -25,11 +25,14 @@ var flipEquiv = function(root1, root2) {
 };
 
 const root1=new TreeNode(1);
+
 root1.left=new TreeNode(2);
 root1.left.left=new TreeNode(4);
 root1.left.right=new TreeNode(5);
 root1.left.right.left=new TreeNode(7);
 root1.left.right.right=new TreeNode(8);
+root1.right=new TreeNode(3);
+root1.right.left=new TreeNode(6);
 
 const root2= new TreeNode(1);
 root2.left=new TreeNode(3);
